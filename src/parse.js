900e50501312710169edc9eff5997c8587647c0f
ctcutler <ctcutler@gmail.com>
2018-05-21 21:51:05 -0400

    basic parsing

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,123 @@
-import R from 'ramda';
-const parse = input => console.log(input);
+import R from "ramda";
+
+const appendToProp = (prop, o) => v => R.assoc(
+    prop, R.append(v, o[prop]), o
+);
+const accountRE = /^account (.*)$/;
+const commodityRE = /^commodity (.*)$/;
+const priceRE = /^P (\S+ \S+) (\S+) \$(\d+\.\d+)$/;
+const commentRE = /^;.*$/;
+const transactionRE = /^(\d{4}\/\d{2}\/\d{2}) ([^\n]+)\n([\s\S]+?)$/;
+// commodity is prefixed
+const prefixPostingRE = /^\s{2,}(.+\S)\s{2,}([^\d\.])([\d\.]+)$/;
+// commodity is postfixed
+const postfixPostingRE = /^\s{2,}(.+\S)\s{2,}([\d\.]+) (.+)$/;
+const implicitPostingRE = /^\s{2,}(.+)$/;
+const pricedPostingRE = /^\s{2,}(.+)\S\s{2,}(.+) (.+) @ (.+)$/;
+const linesAndTransactionsRE = /\n(?=\S)|\n\n/;
+
+const parseAccount = parsed => R.compose(
+  appendToProp("accounts", parsed),
+  R.nth(1),
+  R.match(accountRE)
+);
+const parseCommodity = parsed => R.compose(
+  appendToProp("commodities", parsed),
+  R.nth(1),
+  R.match(commodityRE)
+);
+const parsePrice = parsed => R.compose(
+  appendToProp("prices", parsed),
+  groups => ({"date": groups[1], "symbol": groups[2], "price": groups[3]}),
+  R.match(priceRE)
+);
+
+const append = R.flip(R.append);
+
+const parsePrefixPosting = R.compose(
+  groups => ({
+    "account": groups[1],
+    "commodity": groups[2],
+    "amount": groups[3],
+    "price": ""
+  }),
+  R.match(prefixPostingRE)
+);
+
+const parsePostfixPosting = R.compose(
+  groups => ({
+    "account": groups[1],
+    "commodity": groups[3],
+    "amount": groups[2],
+    "price": ""
+  }),
+  R.match(postfixPostingRE)
+);
+
+const parsePricedPosting = R.compose(
+  groups => ({
+    "account": groups[1],
+    "commodity": groups[3],
+    "amount": groups[2],
+    "price": groups[4]
+  }),
+  R.match(pricedPostingRE)
+);
+
+const parseImplicitPosting = R.compose(
+  groups => ({
+    "account": groups[1],
+    "commodity": "",
+    "amount": "",
+    "price": ""
+  }),
+  R.match(implicitPostingRE)
+);
+
+const parsePostings = R.compose(
+  R.reduce((acc, val) =>
+    R.compose(
+      append(acc),
+      R.cond([
+        [ R.test(pricedPostingRE), parsePricedPosting ],
+        [ R.test(postfixPostingRE), parsePostfixPosting ],
+        [ R.test(prefixPostingRE), parsePrefixPosting ],
+        [ R.test(implicitPostingRE), parseImplicitPosting ],
+      ])
+    )(val),
+    []
+  ),
+  R.split("\n")
+);
+
+const parseTransaction = parsed => R.compose(
+  appendToProp("transactions", parsed),
+  groups => ({
+    "date": groups[1],
+    "desc": groups[2],
+    "postings": parsePostings(groups[3])
+  }),
+  R.match(transactionRE)
+);
+const parseLine = (acc, val) => R.cond([
+  [R.test(accountRE), parseAccount(acc)],
+  [R.test(commodityRE), parseCommodity(acc)],
+  [R.test(priceRE), parsePrice(acc)],
+  [R.test(transactionRE), parseTransaction(acc)],
+  [R.test(commentRE), R.always(acc)],
+  [R.T, R.always(acc)]
+])(val);
+
+const parsedLines = {
+  "accounts": [],
+  "commodities": [],
+  "prices": [],
+  "transactions": [],
+}
+const parse = R.compose(
+  R.reduce(parseLine, parsedLines),
+  R.split(linesAndTransactionsRE)
+);
+
+
 export { parse };
--- src/parse.test.js
+++ src/parse.test.js
@@ -2,7 +2,6 @@ import { parse } from './parse';
 import fs from 'fs';
 
 it('parses', () => {
-  const fileContent = fs.readFileSync('src/App.js', 'utf8');
-  console.log(fileContent);
+  const fileContent = fs.readFileSync('accounts.dat', 'utf8');
   parse(fileContent);
 });
