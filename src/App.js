f29c183380e468ae94fda1c006e88a1655e1f3ba
ctcutler <ctcutler@gmail.com>
2018-08-07 16:23:41 -0400

    asset chart cleanup

--- src/App.js
+++ src/App.js
@@ -9,31 +9,47 @@ import React, { Component } from 'react';
 
 class App extends Component {
   componentDidMount() {
-    const ctx = document.getElementById("myChart").getContext('2d');
     const { prices, transactions } = hydrate(parsed);
+    const granularity = 'month';
     const dataPairs = R.compose(
       //filterByTime(new Date("2017/01/01"), new Date("2017/01/31")),
       accumulateValues,
-      //addEmptyPoints('day', new Date("2017/01/01"), new Date("2017/01/31")),
-      dataPoints('year'),
+      addEmptyPoints(granularity, new Date("2014/01/01"), new Date("2018/09/01")),
+      dataPoints(granularity),
       balance,
       toDollars(prices),
       filterByAccount(/^Assets/)
     )(transactions);
-    const labels = R.compose(R.map(x => x.toString()), R.pluck(0))(dataPairs);
-    const data = R.compose(R.map(x => x.valueOf()), R.pluck(1))(dataPairs);
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels,
-            datasets: [{ data }]
-        },
-    });
+
+    /* Input: format function, index, list of data points
+     * Output: just the item at index of each data point, after format function has been applied
+     */
+    const series = (format, idx, pairs) => R.compose(R.map(format), R.pluck(idx))(pairs);
+    const formatDate = R.invoker(1, "toLocaleDateString")("en-US");
+    const formatDecimal = R.invoker(0, 'valueOf');
+    const labels = series(formatDate, 0, dataPairs);
+    const assetSeries = series(formatDecimal, 1, dataPairs);
+    const datasets = [
+      {
+        data: assetSeries,
+        label: "Assets"
+      }
+    ];
+    const data = { labels, datasets };
+    const options = {
+      elements: {
+        line: {
+          tension: 0, // disables bezier curves
+        }
+      }
+    };
+    const ctx = document.getElementById("myChart").getContext('2d');
+    new Chart(ctx, { type: 'line', data, options });
   }
   render() {
     return (
       <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
+        <canvas id="myChart"></canvas>
       </div>
     );
   }
